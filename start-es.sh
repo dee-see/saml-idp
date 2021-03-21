991953f8461b0ab9a2c0616b9c4f653854c162e1
dcouture <dcouture@gitlab.com>
2021-03-20 23:01:11 -0400

    helper scripts

---
+++ start-es.sh
@@ -0,0 +1,1 @@
+node app.js --acs 'https://localhost:5601/' --aud 'https://localhost:5601/' --host 0.0.0.0 --https --httpsCert ~/cert/idp/idp.pem --httpsPrivateKey ~/cert/idp/idp-key.pem --issuer urn:baffin:idp
