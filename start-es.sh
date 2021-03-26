8ff4f1a5a0575f00ed7cafb1e7cae0c1ae1fea67
dee-see <dominic.couture@gmail.com>
2021-03-25 23:26:37 -0400

    bootstrapping script

--- start-es.sh
+++ start-es.sh
@@ -1,1 +1,23 @@
-node app.js --acs 'https://localhost:5601/' --aud 'https://localhost:5601/' --host 0.0.0.0 --https --httpsCert ~/cert/idp/idp.pem --httpsPrivateKey ~/cert/idp/idp-key.pem --issuer urn:baffin:idp
+#!/bin/bash
+
+cert_dir="$(dirname "$0")/cert"
+if [ ! -d "$cert_dir" ]; then
+    mkdir "$cert_dir"
+    cd "$cert_dir"
+
+    openssl req -x509 -new -newkey rsa:2048 -nodes \
+        -subj '/C=US/ST=California/L=San Francisco/O=JankyCo/CN=Test Identity Provider' \
+        -keyout idp-private-key.pem \
+        -out idp-public-cert.pem -days 7300
+
+    openssl req -x509 -new -newkey rsa:2048 -nodes \
+        -subj '/C=US/ST=California/L=San Francisco/O=JankyCo/CN=Test Identity Provider' \
+        -keyout https-private-key.pem \
+        -out https-public-cert.pem -days 7300
+
+    cd ..
+fi
+
+node app.js --acs 'https://localhost:5601/' --aud 'https://localhost:5601/' --issuer urn:testsamldip:idp \
+    --cert "$cert_dir/idp-public-cert.pem" --key "$cert_dir/idp-private-key.pem" \
+    --host 0.0.0.0 --https --httpsCert "$cert_dir/https-public-cert.pem" --httpsPrivateKey "$cert_dir/https-private-key.pem"
